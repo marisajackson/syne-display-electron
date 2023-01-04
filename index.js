01c493e6d3809a25384b4ecc71408bc563018f99
MaRisa Jackson <mhljackson@gmail.com>
2023-01-04 10:15:58 -0600

    Proof of Concept

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+  win.webContents.openDevTools();
+
+  win.loadFile('index.html');
+}
---
+++ scripts/renderer.js
@@ -0,0 +1,58 @@
+jQuery(function () {
+  let isActive = false;
+
+  $('#start-button').on('click', () => {
+    isActive = !isActive;
+    if(isActive){
+      getCurrentSong();
+      $('#start-button').text('Stop');
+    } else {
+      $('#start-button').text('Start');
+    }
+  });
+
+  function getCurrentSong(){
+    console.log('gettingCurrentSong');
+    $.ajax({
+      url: 'https://syne.herokuapp.com/api/v1/songs/current',
+      type: 'GET',
+      dataType: 'json',
+      data: { songId: $('#spotify-track-id').text() },
+    })
+    .done(function (data) {
+      // window.location.replace(data.spotify_auth_url)
+      document.getElementById("spotify-data").innerHTML = JSON.stringify(data, null, 4);
+      console.log('success')
+      setTrackInfoGUI(data);
+      setTimeout(() => {
+        if(isActive){
+          getCurrentSong();
+        }
+      }, 3000);
+    })
+    .fail(function (err) {
+      console.log(err)
+      console.log('error')
+    })
+    .always(function () {
+      console.log('complete')
+    })
+  }
+
+  function setTrackInfoGUI(data){
+    if(!data){
+      $('#no-song').show();
+    }
+
+    $('#no-song').hide();
+    // result.currently_playing_type: ["track", "?podcast?"]
+    // result.item.[id,name]: song info
+    // result.item.artists[].[id,name]: array of artist info
+    // result.item.album.images[].url
+    $('#album-image').attr('src', data.result.item.album.images[0].url);
+    $('#track-name').text(data.result.item.name);
+    $('#artist-name').text(data.result.item.artists[0].name);
+    $('#album-name').text(data.result.item.album.name);
+    $('#spotify-track-id').text(data.result.item.id);
+  }
+});
